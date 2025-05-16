606b655d7313fb112f300d9882b73abbc2414166
NicolasCV77 <ni.castaneda@javeriana.edu.co>
2025-05-16 08:32:26 -0500

    Agrego Validador.java

---
+++ demo/src/main/java/com/example/Validador.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Validador {
+    public boolean esPositivo(int numero) {
+        return numero > 0;
+    }
+    
+    public boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+}
